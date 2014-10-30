f134df7548f2449f6e192437c8b293376316f39f
Kyle Bennett <kyle@sliktrik.com.au>
2014-10-30 11:46:41 +1100

    drinks added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+ 
+   $('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+                     
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards                             
+        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+         
+        // background position
+        var coords = yPos + 'px';
+ 
+        // move the background
+        $scroll.css({ backgroundPositionY: coords });    
+      }); // end window scroll
+   });  // end section function
     
-    /*var mySwiper = new Swiper('.swiper-container',{
-        pagination: '.pagination',
-        paginationClickable: true
-    });*/
-    
-});
+    $(window).trigger('scroll');
+}); // close out script
