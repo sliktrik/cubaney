95ab308878e71b3002f0df86c791aa63c1da2584
sliktrik <kyle@sliktrik.com.au>
2014-12-17 14:49:48 +1100

    Stockist update

--- js/main.js
+++ js/main.js
@@ -1,45 +1,48 @@
+var menuOpen = false;
+
 $(document).ready(function(){
     
    $window = $(window);
- 
-   $('section[data-type="background"]').each(function(){
-     var $scroll = $(this);
-                     
-      $(window).scroll(function() {                             
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-        var coords = yPos + 'px';
-          
-        $scroll.css({ backgroundPositionY: coords });    
-      });
-   });
-    
-    $(window).trigger('scroll');
-    
-    google.maps.event.addDomListener(window, 'load', init);
     
     scrollNav();
-
-});
-
-function init() {
     
-    var initialLocation = new google.maps.LatLng(-33.8674869, 151.2069902);
+    var h = $("html").css("height");
+    h.replace("px","");
     
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    if($.cookie('userAccepted')){
+        $(".age-gate").hide();
+    }
+    else{
+        $(".age-gate").css("height", h);
+        $(".age-gate").show();
+        
+        $(".button-agree").click(function(){
+            $.cookie('userAccepted', true);
+            $(".age-gate").hide();
+            window.scrollTo(0,0);
+        });
+
+        $(".button-disagree").click(function(){
+            window.location = "http://google.com";
         });
     }
     
-    var mapOptions = {
-        zoom: 11,
-        center: initialLocation, 
-        styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
-    };
+    $(".button-products-nav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".product-section").offset().top - 75},
+            'slow');
+    });
     
-    var mapElement = document.getElementById('map');
-    var map = new google.maps.Map(mapElement, mapOptions);
-}
+    $(".button-products-nav-m").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".product-section").offset().top},
+            'slow');
+    });
+    
+    $(".hamburger-menu-icon").click(function(){
+        toggleMobileMenu();
+    });
+});
 
 function scrollNav() {
   $('nav a').click(function(){  
@@ -48,11 +51,46 @@ function scrollNav() {
     $(this).closest('li').addClass("active");
     var theClass = $(this).attr("class");
     $('.'+theClass).parent('li').addClass('active');
-    //Animate
+      
+    var offset = 75;  
+      
+    if(window.innerWidth < 600){
+        offset = 0;
+        toggleMobileMenu();   
+    }
+      
     $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 75
+        scrollTop: $( $(this).attr('href') ).offset().top - offset
     }, 400);
     return false;
   });
+    
   $('.scrollTop a').scrollTop();
+}
+
+function scrollSmoke(){
+    
+    var xposraw = $(".smoke img").css('left');
+    var xpos = xposraw.replace("px","");
+    
+    xpos = xpos - 1;
+    
+    console.log(xpos);
+    
+    $(".smoke img").css("left", xpos + "px");
+}
+
+function toggleMobileMenu(){
+    
+    if(menuOpen){
+        $("nav").fadeOut(250,function(){
+            menuOpen = false;
+        });
+        
+    }else{
+        $("nav").css('height',window.innerHeight+"px");
+        $("nav").css('padding-top',(window.innerHeight/4)+"px");
+        $("nav").fadeIn(500);
+        menuOpen = true;
+    }
 }
--- js/storelocator.js
+++ js/storelocator.js
@@ -13,17 +13,52 @@ google.maps.event.addDomListener(window, 'load', function() {
     
     var initialLocation = new google.maps.LatLng(-33.8674869, 151.2069902);
     
-    if (navigator.geolocation) 
+    /*if (navigator.geolocation) 
     {
         navigator.geolocation.getCurrentPosition(function (position) {
             initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         });
-    }
+    }*/
     
     var mapOptions = {
         zoom: 11,
+        scrollwheel: false,
         center: initialLocation, 
-        styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
+        panControl: false,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
+        
+        styles: [
+            {
+                "stylers": [
+                    {
+                        "hue": "#ff1a00"
+                    },
+                    {
+                        "invert_lightness": true
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 33
+                    },
+                    {
+                        "gamma": 0.5
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    }
+                ]
+            }
+        ]
     };
     
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
